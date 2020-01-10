459e23e3d0188325bbd07c54592e9bbaa424b49c
Krishna Mohan <krishna.mohan@cleartrip.com>
2020-01-10 22:02:19 +0530

    initialized

---
+++ src/test/java/com/cvr/it/coderunner/CoderunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvr.it.coderunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
