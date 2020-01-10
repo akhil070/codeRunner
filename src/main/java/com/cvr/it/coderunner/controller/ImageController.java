1b2c796ec0cc5f7f13e5f82f564663be9256e751
Krishna Mohan <krishna.mohan@cleartrip.com>
2020-01-10 22:07:08 +0530

    lombok

---
+++ src/main/java/com/cvr/it/coderunner/controller/ImageController.java
@@ -0,0 +1,11 @@
+package com.cvr.it.coderunner.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author krishnamohan
+ * @date 10/01/20
+ **/
+
+@Controller
+public class ImageController {}
