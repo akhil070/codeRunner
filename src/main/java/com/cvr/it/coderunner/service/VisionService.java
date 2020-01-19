2c6381ad8907d864932b6ae49d9f823ae905972d
Krishna Mohan <krishna.mohan@cleartrip.com>
2020-01-19 10:00:06 +0530

    removed keys

---
+++ src/main/java/com/cvr/it/coderunner/service/VisionService.java
@@ -0,0 +1,67 @@
+package com.cvr.it.coderunner.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+
+import com.amazonaws.services.textract.model.BadDocumentException;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author krishnamohan
+ * @date 17/01/20
+ **/
+
+@Service
+@Slf4j
+public class VisionService {
+    
+    ImageAnnotatorClient client = null;
+    
+    @PostConstruct
+    public void init() {
+        try {
+            client = ImageAnnotatorClient.create();
+            log.info("GCP options initialized successfully");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    
+    public String detectText(MultipartFile file) throws Exception, IOException {
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+        
+        Image img = Image.newBuilder().setContent(ByteString.copyFrom(file.getBytes())).build();
+        Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+        AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+        
+        BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+        List<AnnotateImageResponse> responses = response.getResponsesList();
+        
+        StringBuilder predicted = new StringBuilder();
+        
+        for (AnnotateImageResponse res : responses) {
+            if (res.hasError()) {
+                log.error("Error: {}\n", res.getError().getMessage());
+                throw new BadDocumentException(res.getError().getMessage());
+            }
+            
+            // For full list of available annotations, see http://g.co/cloud/vision/docs
+            res.getTextAnnotationsList().stream().map(text -> predicted.append(text.getDescription()));
+        }
+        
+        return predicted.toString();
+    }
+    
+}
