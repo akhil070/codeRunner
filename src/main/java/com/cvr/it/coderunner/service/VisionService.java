0f1bafeab5c6f768d2ac9722309d00fe70e08eca
Krishna Mohan <krishna.mohan@cleartrip.com>
2020-03-05 03:37:37 +0530

    template used for vision

--- src/main/java/com/cvr/it/coderunner/service/VisionService.java
+++ src/main/java/com/cvr/it/coderunner/service/VisionService.java
@@ -1,26 +1,12 @@
 package com.cvr.it.coderunner.service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import javax.annotation.PostConstruct;
 
-import com.amazonaws.services.textract.model.BadDocumentException;
-import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
-import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
-import com.google.cloud.vision.v1.EntityAnnotation;
 import com.google.cloud.vision.v1.Feature;
-import com.google.cloud.vision.v1.Image;
-import com.google.cloud.vision.v1.ImageAnnotatorClient;
-import com.google.protobuf.ByteString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.multipart.MultipartFile;
 import lombok.extern.slf4j.Slf4j;
 
