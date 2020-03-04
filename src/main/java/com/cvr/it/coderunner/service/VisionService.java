a025c7f7274a3eda36cc95105ee626022c62a3b1
Krishna Mohan <krishna.mohan@cleartrip.com>
2020-03-05 03:37:07 +0530

    template used for vision

--- src/main/java/com/cvr/it/coderunner/service/VisionService.java
+++ src/main/java/com/cvr/it/coderunner/service/VisionService.java
@@ -2,18 +2,25 @@ package com.cvr.it.coderunner.service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 
 import com.amazonaws.services.textract.model.BadDocumentException;
 import com.google.cloud.vision.v1.AnnotateImageRequest;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
 import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
 import com.google.cloud.vision.v1.Feature;
 import com.google.cloud.vision.v1.Image;
 import com.google.cloud.vision.v1.ImageAnnotatorClient;
 import com.google.protobuf.ByteString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.multipart.MultipartFile;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,43 +33,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class VisionService {
     
-    ImageAnnotatorClient client = null;
-    
-    @PostConstruct
-    public void init() {
-        try {
-            client = ImageAnnotatorClient.create();
-            log.info("GCP options initialized successfully");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+    @Autowired
+    private CloudVisionTemplate template;
     
     public String detectText(MultipartFile file) throws Exception, IOException {
-        List<AnnotateImageRequest> requests = new ArrayList<>();
         
-        Image img = Image.newBuilder().setContent(ByteString.copyFrom(file.getBytes())).build();
-        Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
-        AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
-        requests.add(request);
+        AnnotateImageResponse response = template.analyzeImage(file.getResource(), Feature.Type.TEXT_DETECTION);
+        String textFromImage = this.template.extractTextFromImage(file.getResource());
+        return textFromImage;
         
-        BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
-        List<AnnotateImageResponse> responses = response.getResponsesList();
-        
-        String predicted = null;
-        
-        for (AnnotateImageResponse res : responses) {
-            if (res.hasError()) {
-                log.error("Error: {}\n", res.getError().getMessage());
-                throw new BadDocumentException(res.getError().getMessage());
-            }
-            
-            // For full list of available annotations, see http://g.co/cloud/vision/docs
-            predicted = res.getTextAnnotationsList().get(0).getDescription();
-//            res.getTextAnnotationsList().stream().map(text -> predicted.append(text.getDescription()));
-        }
-        
-        return predicted;
     }
     
 }
